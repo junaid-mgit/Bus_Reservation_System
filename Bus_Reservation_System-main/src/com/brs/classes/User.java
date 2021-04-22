3000631f292d4a7b74b55c77852d605fc47b404b
Nishtha Bhopatkar <nishtha.bhopatkar@gmail.com>
2021-04-22 09:03:11 +0530

    Sign up

---
+++ Bus_Reservation_System-main/src/com/brs/classes/User.java
@@ -0,0 +1,83 @@
+package com.brs.classes;
+
+import java.util.Objects;
+
+public class User
+{
+    private String fname;
+    private String lname;
+    private String email;
+    private String password;
+
+    public User(String fname, String lname, String email, String password) {
+        this.fname = fname;
+        this.lname = lname;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getFname() {
+        return fname;
+    }
+
+    public void setFname(String fname) {
+        this.fname = fname;
+    }
+
+    public String getLname() {
+        return lname;
+    }
+
+    public void setLname(String lname) {
+        this.lname = lname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" + "fname=" + fname + ", lname=" + lname + ", email=" + email + ", password=" + password + '}'+"\n";
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final User other = (User) obj;
+        if (!Objects.equals(this.fname, other.fname)) {
+            return false;
+        }
+        if (!Objects.equals(this.lname, other.lname)) {
+            return false;
+        }
+        if (!Objects.equals(this.email, other.email)) {
+            return false;
+        }
+        if (!Objects.equals(this.password, other.password)) {
+            return false;
+        }
+        return true;
+    }
+    
+}
