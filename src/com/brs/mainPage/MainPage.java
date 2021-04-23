b09e8e5ef62c47ec39edda2384e1dd60d9eb95e5
Chirag-Gupta-057 <37701482+Chirag-Gupta-057@users.noreply.github.com>
2021-04-23 14:23:41 +0530

    Update MainPage.java

--- src/com/brs/mainPage/MainPage.java
+++ src/com/brs/mainPage/MainPage.java
@@ -1,26 +1,6 @@
 package com.brs.mainPage;
 
-import java.awt.BorderLayout;
-import java.awt.Dimension;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 
-import javax.swing.BorderFactory;
-import javax.swing.Box;
-import javax.swing.BoxLayout;
-import javax.swing.JButton;
-import javax.swing.JComboBox;
-import javax.swing.JFormattedTextField;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JTable;
-import javax.swing.JTextField;
 
 public class MainPage {
 
