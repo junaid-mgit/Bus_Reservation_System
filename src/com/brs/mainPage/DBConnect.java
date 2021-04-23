9bee7caf3e33677fe5944509f5492bed46dfd858
Chirag-Gupta-057 <37701482+Chirag-Gupta-057@users.noreply.github.com>
2021-04-23 14:23:00 +0530

    added search bus result method

--- src/com/brs/mainPage/DBConnect.java
+++ src/com/brs/mainPage/DBConnect.java
@@ -57,7 +57,27 @@ public class DBConnect {
 	}
 	// method to return result set of travel areas
 	public ResultSet getTravelAreas() {
-		String query = "SELECT area_name FROM Bus_Reservation_System.busrv_busdroparea;";
+		String query = "SELECT distinct area_name FROM Bus_Reservation_System.busrv_busdroparea union "
+				+ "Select distinct area_name from Bus_Reservation_System.busrv_buspickarea;";
+		
+		try {
+			res.beforeFirst();
+			res = stat.executeQuery(query);
+			res.beforeFirst();
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return res;
+	}
+	// method returns the search result for available buses
+	public ResultSet searchBusesResultSet(String fromArea,String toArea) {
+		String query = "select * from busrv_businfo "
+				+ " where arriving_from_id =any("
+				+ "	select id from busrv_buspickarea where area_name = '" + fromArea + "'"
+				+ " ) and depature_at_id =any("
+				+ "	select id from busrv_busdroparea where area_name = '" + toArea + "'"
+				+ " ) and is_active = 1;";
 		try {
 			res.beforeFirst();
 			res = stat.executeQuery(query);
